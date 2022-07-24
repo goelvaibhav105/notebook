9c1ca8d1ee0c0785480abba9db765c834dda2c42
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-24 14:37:59 +0530

    Notes react App some func added also context added

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,113 @@
+import NoteContext from "./noteContext";
+import { useState } from "react";
+
+const NoteState = (props)=>{
+    const notesInitial = [
+        {
+          "_id": "61322f195153781a8ca8d0e06",
+          "user": "6131dc5e3e4037cd4734a066",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2021-09-03T14:20:09.509Z",
+          "__v": 0
+        },
+        {
+          "_id": "61322f195531781a8ca8d0e08",
+          "user": "6131dc5e3e4037cd4734a066",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2021-09-03T14:20:09.668Z",
+          "__v": 0
+        },
+        {
+          "_id": "61322f19553781a8ca8d0e081",
+          "user": "6131dc5e3e4037cd4734a066",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2021-09-03T14:20:09.668Z",
+          "__v": 0
+        },
+        {
+          "_id": "61322f19553781a8ca8d0e082",
+          "user": "6131dc5e3e4037cd4734a066",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2021-09-03T14:20:09.668Z",
+          "__v": 0
+        },
+        {
+          "_id": "61322f195537812a8ca8d0e08",
+          "user": "6131dc5e3e4037cd4734a066",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2021-09-03T14:20:09.668Z",
+          "__v": 0
+        },
+        {
+          "_id": "613222f19553781a8ca8d0e08",
+          "user": "6131dc5e3e4037cd4734a066",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2021-09-03T14:20:09.668Z",
+          "__v": 0
+        },
+        {
+          "_id": "61322f119553781a8ca8d0e08",
+          "user": "6131dc5e3e4037cd4734a066",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2021-09-03T14:20:09.668Z",
+          "__v": 0
+        },
+      ]
+      const [notes, setNotes] = useState(notesInitial)
+
+
+      // Add a Note
+      const addNote = (title, description, tag)=>{
+        // TODO: API Call
+        console.log("Adding a new note")
+
+        // see only 3 items are variable as other items are selfgenrating in our API 
+        const note = {
+          "_id": "61322f119553781a8ca8d0e08",
+          "user": "6131dc5e3e4037cd4734a0664",
+          "title": title,
+          "description": description,
+          "tag": tag,
+          "date": "2021-09-03T14:20:09.668Z",
+          "__v": 0
+        };
+        // concat returns an array whereas push updates an array 
+        setNotes(notes.concat(note)) 
+      }
+
+      // Delete a Note
+      const deleteNote = (id)=>{
+        // TODO: API Call
+        console.log("Deleting the note with id" + id);
+        const newNotes = notes.filter((note)=>{return note._id!==id})
+        setNotes(newNotes)
+      }
+
+      // Edit a Note
+      const editNote = (id, title, description, tag)=>{
+
+      }
+    
+    return (
+      // here in value we are only passing states which we can use in our note application anywhere 
+        <NoteContext.Provider value={{notes, addNote,deleteNote, editNote }}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState;
