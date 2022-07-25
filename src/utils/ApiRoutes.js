562ebaeddb530e5e9159cc51340122cc85318387
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-25 15:06:51 +0530

    routes

---
+++ src/utils/ApiRoutes.js
@@ -0,0 +1,8 @@
+export const host = "https://daily-notes-vaibhav.herokuapp.com";
+export const createUserRoute = `${host}/api/auth/createuser`;
+export const loginRoute = `${host}/api/auth/login`;
+export const getAllNotesRoute = `${host}/api/notes/fetchallnotes`;
+export const updateNoteRoute = `${host}/api/notes/updatenote`;
+export const deleteNoteRoute = `${host}/api/notes/deletenote`;
+export const addNoteRoute = `${host}/api/notes/addnote`;
+
